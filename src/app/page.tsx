d13b5b5327ed7f4af9f98fb2d942e4734299d4b0
donjne <davidjrn247@gmail.com>
2025-01-23 17:31:41 +0100

    h2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import Navbar from '@/components/Navbar';
 import Header from '@/components/Header';
+import FeatureCards from '@/components/FeatureCards';
 
 export default function Home() {
   return (
     <main>
       <Navbar />
       <Header />
+      <div className="relative" style={{ marginTop: '400px' }}>
+        <FeatureCards />
+      </div>
     </main>
   );
 }
